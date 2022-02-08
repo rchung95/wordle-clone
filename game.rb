13e273a1ef7f1aa6e91d88372a3d4c73f3c14bdd
Raymond Chung <raymondchung@raymonds-mbp.lan>
2022-02-07 23:33:13 -0500

    Adding paint

--- game.rb
+++ game.rb
@@ -1,10 +1,13 @@
+require 'paint'
+
 class Game
-  attr_accessor :total_attempts, :words, :active
+  attr_accessor :attempt, :words, :active, :current_word
   attr_reader :word, :max_attempts
 
   def initialize
     @word = ["t", "o", "d", "a", "y"]
-    @total_attempts = 0
+    @current_word = ""
+    @attempt = 0
     @max_attempts = 5
     @words = [
       ["_", "_", "_", "_", "_"],
@@ -16,27 +19,49 @@ class Game
     @active = true
   end
 
+  def board
+    @words.each do |row|
+      puts row.join(" ")
+    end
+  end
+
   def get_word
-    puts "Enter your word: "
+    puts Paint["Enter your word: ", :blue]
     new_word = gets.chomp
     if new_word.length == 5 
-      @words[total_attempts] = new_word.split("")
+      @current_word = new_word.split("")
+      add_word_to_board
     else
-      puts "Your word must be 5 characters long."
+      puts Paint["Your word must be 5 characters long.", :red]
     end
   end
 
   def check_word
-    if @words[total_attempts] == @word
+    if @current_word == @word
       @active = false
-      return "Congratulations! You solved the word \"#{@word.join("")}\" on total_attempts #{@total_attempts + 1}."
+      return "Congratulations! You solved the word #{Paint[@word.join(""), :green]} on attempt #{Paint[@attempt + 1, :green]}."
     else
       return response
     end
   end
 
+  def add_word_to_board
+    colorized_word = []
+    @current_word.each_with_index do |char, index|
+      if char == @word[index]
+        colorized_word << Paint[char, :green]
+      elsif @word.include?(char)
+        colorized_word << Paint[char, :yellow]
+      else
+        colorized_word << char
+      end
+    end
+
+    @words[@attempt] = colorized_word
+  end
+
   def active?
-    @total_attempts < @max_attempts && @active
+    @attempt < @max_attempts && @active
   end
 
   private
@@ -44,15 +69,15 @@ class Game
   def response
     response = "Result:\n"
 
-    @words[total_attempts].each_with_index do |char, index|
+    @current_word.each_with_index do |char, index|
       if @word[index] == char
-        response += "- \"#{char}\" is correct in position #{index}.\n"
+        response += "- #{Paint[char, :green]} is correct in position #{index}.\n"
       elsif @word.include?(char)
-        response += "- \"#{char}\" is in the word but not in the correct position. Or its already in the correct position.\n"
+        response += "- #{Paint[char, :yellow]} is in the word but not in the correct position. Or its already in the correct position.\n"
       end
     end
 
-    @total_attempts += 1
+    @attempt += 1
 
     response
   end
--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@ require_relative 'game.rb'
 
 game = Game.new
 while game.active?
+  game.board
   game.get_word
   puts game.check_word
 end
+game.board
