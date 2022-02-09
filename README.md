71dc170f599c28198c261346fea426a075ccea81
Raymond Chung <raymondchung@raymonds-mbp.lan>
2022-02-08 19:52:22 -0500

    Added more words and fixed logic

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Wordle
 
-A program created in Ruby based on the popular Wordle game. This program is still a WIP.
+A program created in Ruby based on the popular Wordle game. Unlike the original game, there is only 5 attempts instead of 6.
+
+## Sample runthrough
+![Sample runthrough](/assets/sample-runthrough.png)
+
+## How to run the game.
+```bash
+ruby main.rb
+```
+
+## Gems used
+* [Paint](https://github.com/janlelis/paint)
--- game.rb
+++ game.rb
@@ -1,52 +1,73 @@
 require 'paint'
+require 'yaml'
 
 class Game
   attr_accessor :attempt, :words, :active, :current_word
   attr_reader :word, :max_attempts
 
   def initialize
-    @word = ["t", "o", "d", "a", "y"]
+    @word = YAML.load_file("words.yml").sample
     @current_word = ""
     @attempt = 0
     @max_attempts = 5
     @words = [
-      ["_", "_", "_", "_", "_"],
-      ["_", "_", "_", "_", "_"],
-      ["_", "_", "_", "_", "_"],
-      ["_", "_", "_", "_", "_"],
-      ["_", "_", "_", "_", "_"],
+      "_ _ _ _ _",
+      "_ _ _ _ _",
+      "_ _ _ _ _",
+      "_ _ _ _ _",
+      "_ _ _ _ _",
     ]
     @active = true
   end
 
+  def intro
+    Paint["Welcome to Wordle! You got #{max_attempts} attempts to guess the correct 5 letter word!", :magenta]
+  end
+
   def board
     @words.each do |row|
-      puts row.join(" ")
+      puts row
+    end
+  end
+
+  def get_input
+    invalid = true
+    while invalid
+      new_word = gets.chomp.gsub(/\s+/, "")
+      
+      if new_word.length == 5
+        @current_word = new_word.downcase.split("")
+        invalid = false
+      else
+        puts Paint["Your word must be 5 characters long. Try again:", :red]
+      end
     end
   end
 
   def get_word
     puts Paint["Enter your word: ", :blue]
-    new_word = gets.chomp
-    if new_word.length == 5 
-      @current_word = new_word.split("")
-      add_word_to_board
-    else
-      puts Paint["Your word must be 5 characters long.", :red]
+    get_input
+    add_word_to_board
+  end
+
+  def check_attempt
+    if @attempt == @max_attempts && !active?
+      @active = false
+      puts Paint["Game over! You have used up all attempts. Today's word is #{Paint[@word, :cyan]}.\n", :red]
     end
   end
 
   def check_word
-    if @current_word == @word
+    if @current_word.join("") == @word
       @active = false
-      return "Congratulations! You solved the word #{Paint[@word.join(""), :green]} on attempt #{Paint[@attempt + 1, :green]}."
+      return "Congratulations! You solved the word #{Paint[@word, :green]} on attempt #{Paint[@attempt + 1, :green]}."
     else
       return response
     end
   end
 
   def add_word_to_board
-    colorized_word = []
+    colorized_word = ""
     @current_word.each_with_index do |char, index|
       if char == @word[index]
         colorized_word << Paint[char, :green]
@@ -55,6 +76,7 @@ class Game
       else
         colorized_word << char
       end
+      colorized_word << " "
     end
 
     @words[@attempt] = colorized_word
